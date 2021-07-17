673f38ca0e4d84539f0ec789cb5631c7579d8207
EddyVL3 <leoy23@hotmail.com>
2021-07-17 15:12:59 +0700

    Variance
    Set Variance

--- Exercise4_Voravit_L.py
+++ Exercise4_Voravit_L.py
@@ -1,4 +1,9 @@
 print ('hello world')
+FE = 'Foundation English'
+GB = 'General Business'
+ICS = 'Introduction to Computer Systems'
+CP = 'Computer Programming'
+
 print ('-----Voravit Liammongkhonkul-----')
 print ('-----Your Score-----')
 print (FE,'-----',89.50,'คะแนน')
