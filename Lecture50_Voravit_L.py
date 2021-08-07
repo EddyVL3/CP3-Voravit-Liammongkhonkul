8c04b6ccf72f5bdffb4e4f39fb7e0735dc582dc8
EddyVL3 <leoy23@hotmail.com>
2021-08-07 22:17:34 +0700

    Function_Lecture50

---
+++ Lecture50_Voravit_L.py
@@ -0,0 +1,14 @@
+First_number = int(input("First Number : "))
+Second_number = int(input("Second Number : "))
+def Plus (First_number,Second_number):
+    print (First_number+Second_number)
+def Minus (First_number,Second_number):
+    print (First_number-Second_number)
+def Multiply (First_number,Second_number):
+    print (First_number*Second_number)
+def Divide (First_number,Second_number):
+    print (First_number/Second_number)
+Plus(First_number,Second_number)
+Minus(First_number,Second_number)
+Multiply(First_number,Second_number)
+Divide(First_number,Second_number)
